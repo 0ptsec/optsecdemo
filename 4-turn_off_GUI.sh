f802f7c03cb447dea28bbd6104fa8722de983d59
optsec <91828789+0ptsec@users.noreply.github.com>
2021-10-05 12:09:12 -0400

    Add files via upload

---
+++ 4-turn_off_GUI.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -e "$(tput setaf 3)$(tput bold)===============================Running turn_off_GUI.sh=================================$(tput sgr0)\n"
+
+
+sudo echo 'gpu_mem=16' | sudo tee -a /boot/config.txt >/dev/null
+sudo update-rc.d lightdm disable
+echo -e "$(tput setaf 1)$(tput bold)*****MAKE NOTE OF THE FOLLOWING*****$(tput sgr0)\n"
+echo -e "$(tput setaf 1)$(tput bold)Disable desktop in System Options ---> Boot / Auto Logon by selecting B1 - Text console, requiring user to logon.$(tput sgr0)\n"
+echo -e "$(tput setaf 1)$(tput bold)You'll be asked to reboot afterwards, say yes.$(tput sgr0)\n"
+read -n 1 -s -r -p "After reading, press any key to continue"
+echo -e "\n"
+sudo raspi-config
+
+echo "$(tput setaf 3)$(tput bold)===================== All Done======================$(tput sgr0)"
