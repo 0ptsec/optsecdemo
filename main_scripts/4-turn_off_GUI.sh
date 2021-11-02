573efaf7e6f2cab45b271fa870730c0e9eeacdae
optsec <91828789+0ptsec@users.noreply.github.com>
2021-11-02 13:00:10 -0400

    Update 4-turn_off_GUI.sh

--- main_scripts/4-turn_off_GUI.sh
+++ main_scripts/4-turn_off_GUI.sh
@@ -7,7 +7,7 @@ sudo echo 'gpu_mem=16' | sudo tee -a /boot/config.txt >/dev/null
 sudo update-rc.d lightdm disable
 echo -e "$(tput setaf 1)$(tput bold)*****MAKE NOTE OF THE FOLLOWING*****$(tput sgr0)\n"
 echo -e "$(tput setaf 1)$(tput bold)Disable desktop in System Options ---> Boot / Auto Logon by selecting B1 - Text console, requiring user to logon.$(tput sgr0)\n"
-echo -e "$(tput setaf 1)$(tput bold)You'll be asked to reboot afterwards, say yes.$(tput sgr0)\n"
+echo -e "$(tput setaf 1)$(tput bold)You will be asked to Finish and reboot afterwards, say yes to reboot.$(tput sgr0)\n"
 read -n 1 -s -r -p "After reading, press any key to continue"
 echo -e "\n"
 sudo raspi-config
